afacd2fa3fbab2aac9d050634af546014dc2c131
phuonggavt2000 <phuonggavt001@gmail.com>
2022-04-06 18:26:38 +0700

    setting 05

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 let bnt = document.querySelectorAll("button")
+let arrName = Array()
 bnt.forEach(function(button){
     button.addEventListener("click",function() {
         event.target // them su kien
-        var product = this.parentElement // lay cha cua button
-        var productImg = product.querySelector("img").src
-        var productPrice = product.querySelector("span").innerHTML
-        var productName = product.querySelector("h1").innerHTML
+        let product = this.parentElement // lay cha cua button
+        let productImg = product.querySelector("img").src
+        let productPrice = product.querySelector("span").innerHTML
+        let productName = product.querySelector("h1").innerHTML
         addcart(productImg,productPrice,productName)
+        let bnt = document.querySelectorAll("button")
+        // console.log(bnt)
     })
 })
 function addcart(productImg,productPrice,productName) {
-    var addtr = document.createElement("tr")
-    var trcontent = ' <td style="display: flex; align-items: center;"><img style="width: 80px;" src="'+productImg+'"alt=""><span>'+productName+'</span></td><td><p><span id="pr">'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 50px;" type="number" value="1" min="0"></td><td><button onclick="del1()">Xóa</button></td><td id="toltal"></td>'
-    addtr.innerHTML = trcontent
-    var carTable = document.querySelector("tbody")
-    var nameCart = carTable.querySelector("span")
+    let addtr = document.createElement("tr")
+    let trcontent = ' <td style="display: flex; align-items: center;"><img style="width: 80px;" src="'+productImg+'"alt=""><span class="same">'+productName+'</span></td><td><p><span id="pr">'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 50px;" type="number" value="1" min="0"></td><td><button>Xóa</button class="button"></td><td id="toltal"></td>'
+    addtr.innerHTML = trcontent // thêm sản phẩm vào thẻ tr
+    let carTable = document.querySelector("tbody")
+    // for(i = 0; i < carTable.length; i++) {
+    //     let nameSame = carTable.querySelectorAll(".same")
+
+    // }
+//    console.log(carTable)
+ 
+      carTable.append(addtr)
+    let buttonCss = carTable.querySelector("tr button:last-child")
+    buttonCss.className = "button"
+      console.log(carTable)
+    // for (i = 0; i < )
+    // let sameName = carTable.querySelector("span")
     // console.log(nameCart)
     // neu PriductName giong nhau thi input cua value do tang 1 
 // if (productName === nameCart) {
@@ -22,29 +36,76 @@ function addcart(productImg,productPrice,productName) {
 // } else {
     
 // }
-carTable.append(addtr)
+
+
+
+
+// if (productName === )
     
     carttotal() // goi den tong
+deleteproduct()
+let sameName = carTable.querySelector("tr:last-child td span").innerHTML
+arrName.push(sameName)
+
 
 }
-function carttotal() {
-    var cartItem = document.querySelectorAll(".table-body tr")
-    var total = 0
-    for(var i = 0; i < cartItem.length; i++) {
+function carttotal(carTable) {
+    
+    let cartItem = document.querySelectorAll(".table-body tr")
+    let total = 0
+    for(let i = 0; i < cartItem.length; i++) {
         let price = cartItem[i].querySelector("#pr").innerHTML
         let valueInput = cartItem[i].querySelector("input").value
     //   console.log(valueInput)
 total += price*valueInput
     }
     total // tong
-    var toltalTable = document.querySelector("#toltal")
-    var tableTd = document.createElement("td")
-    toltalTable.innerHTML = ""
-    tableTd.innerHTML = total + ".000"
-    toltalTable.append(tableTd)
-    // console.log(tableTd)
+    let addToltal = document.querySelector("table  tr td")
+    let span = document.createElement("span")
+    addToltal.innerHTML = ""
+    span.innerHTML = "Tổng:   " + total + ".000" + "đ"
+    addToltal.append(span)
+    let spanClass = addToltal.querySelector("span")
+    spanClass.className = "toltal"
+    console.log(spanClass)
+    
     
 }
+
+function deleteproduct() {
+    let bntDel = document.querySelectorAll("tbody tr td button")
+for (i = 0; i < bntDel.length; i++) {
+    bntDel[i].addEventListener("click", function() {
+        this.target
+        let parenDel = this.parentElement.parentElement
+        parenDel.remove()
+        carttotal()
+        // console.log(parenDel)
+    })
+// bntDel[i].addEventListener("click",function() {
+//     this.target
+//     let paren = this.parentElement
+//     console.log(paren)
+// })
+}
+}
+
+// function add() {
+//     let addImg = document.querySelector(".contain-add #img").value
+//     let addPrice = document.querySelector(".contain-add #price").value
+//     let addName = document.querySelector(".contain-add #name").value
+//     let addDiv = document.createElement("div")
+//     let containProduct = document.querySelector(".product-items")
+//     let divContain = 
+//     '<img src="'+addImg+'" alt="">'+
+//     '<p><span>'+addPrice+'</span><sup>đ</sup></p>'+
+//     '<h1>'+addName+'</h1>' +
+//     '<button>Thêm giỏ hàng</button>'
+//     addDiv.innerHTML = divContain
+//     addDiv.className = "product-item"
+//     containProduct.append(addDiv)
+//     // console.log(containProduct)
+// }
 // su ly input
 // function addInput(name,table) {
     
@@ -68,7 +129,7 @@ total += price*valueInput
 
 // bnt.forEach(function(button) {
 //     button.addEventListener("click",function(event) {
-//         var btnItem = event.target
+//         let btnItem = event.target
 //         console.log(btnItem)
 //     })
 // })
